a524a48b121c320cf52d02e2a71f8f0db9c8b3e2
RosvoRolle <nuuttiviljamilehtonen@gmail.com>
2025-10-14 23:05:18 +0300

    Liikkumis koodausta

--- Scenes/character_body_2d.gd
+++ Scenes/character_body_2d.gd
@@ -1,2 +1,24 @@
 extends CharacterBody2D
-@onready var
+
+@onready var _animated_sprite = $AnimatedSprite2D
+
+@export var nopeus: float = 600.0
+
+func _process(delta):
+	
+	var suunta := Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	position.x += suunta * nopeus * delta
+	
+	if Input.is_action_pressed("ui_right"):
+		_animated_sprite.play("run")
+		_animated_sprite.flip_h = false
+	
+	elif Input.is_action_pressed("ui_left"):
+		_animated_sprite.play("run")
+		_animated_sprite.flip_h = true	
+	
+	else:
+		_animated_sprite.stop()
+		
+		
+	
